1afbd45ab10d154af9b74dfa54fb32e41f359819
nikitha31 <59816775+nikitha31@users.noreply.github.com>
2020-03-01 15:27:06 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Serialization
+{
+    class Program
+    {
+        public static GenreEnum Romance { get; set; }
+
+        static void Main(string[] args)
+        {
+            string x = "byyhgbuyhda";
+            Books b = new Books("1234", "abc", "scfdf", "jnjn", GenreEnum.Romance, "dcd", new DateTime(2020, 12, 31), "byuyuhhj ", new DateTime(2020, 12, 21));
+            Books b2 = new Books("1234", "abc", "scfdf", "jnjn", GenreEnum.Romance, "dcd", new DateTime(2020, 12, 31), "byuyuhhj ", new DateTime(2020, 12, 21));
+            Books[] b1 = new Books[2] { b, b2 };
+            Catalog c = new Catalog(x, b1);
+
+            // Serialize the object data to a file
+            Stream stream = File.Open("BookOpen.xml", FileMode.Create);
+            XmlSerializer bf = new XmlSerializer(typeof(Catalog));
+
+            // Send the object data to the file
+            bf.Serialize(stream, c);
+            stream.Close();
+
+
+            Catalog catalog = null;
+            FileStream stream1 = File.Open("books1.xml", FileMode.Open);
+            //XmlRootAttribute xRoot = new XmlRootAttribute();
+            //xRoot.ElementName = "catalog";
+            XmlSerializer xs = new XmlSerializer(typeof(Catalog));
+            catalog = (Catalog)xs.Deserialize(stream1);
+            int k = catalog.book.Length;
+            int i = 0;
+            Console.WriteLine("Catalog");
+            Console.WriteLine("Catalog Date:" + catalog.date);
+            while (i < k)
+            {
+                Console.WriteLine("\t" + "Book ID : " + catalog.book[i].id);
+                Console.WriteLine("\t" + "Author : " + catalog.book[i].Author);
+                Console.WriteLine("\t" + "Title : " + catalog.book[i].Title);
+                Console.WriteLine("\t" + "Genre : " + catalog.book[i].Genre);
+                Console.WriteLine("\t" + "Publisher : " + catalog.book[i].Publisher);
+                Console.WriteLine("\t" + "Publish Date : " + catalog.book[i].Publish_Date);
+                Console.WriteLine("\t" + "Description : " + catalog.book[i].Description);
+                Console.WriteLine("\t" + "Registration Date : " + catalog.book[i].Registration_Date);
+                Console.WriteLine();
+                i++;
+            }
+            JsonSerializer jsonSerializer = new JsonSerializer();
+           // Stream sw = File.Open("Boo.json", FileMode.Create);
+            using (var sw = new StreamWriter(@"C:\Users\JEVANI NIKITHA\source\repos\Serialization\Serialization\bin\Debug\boo.json"))
+            using (JsonWriter writer = new JsonTextWriter(sw))
+            {
+                jsonSerializer.Serialize(writer, catalog);
+            }
+            FileStream s1 = File.Open("boo.json", FileMode.Open);
+            Console.WriteLine(s1.ToString());
+           // Stream SS = new NetworkStream(S);
+            StreamReader reader = new StreamReader(s1);
+            string MSG = reader.ReadLine();
+           // Console.WriteLine(MSG);
+            JObject parsed = JObject.Parse(MSG);
+
+            foreach (var pair in parsed)
+            {
+                Console.WriteLine("{0}: {1}", pair.Key, pair.Value);
+            }
+            Console.ReadKey();
+        }
+
+
+public IEnumerator<Catalog> GetEnumerator()
+{
+    return (IEnumerator<Catalog>)this;
+}
+
+public void Reset()
+{
+    throw new NotImplementedException();
+}
+    }
+}
---
+++ Serialization.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Serialization
+{
+	public enum GenreEnum
+	{
+		Computer,
+		Fantasy,
+		Romance,
+		Horror,
+		ScienceFiction
+	}
+	[Serializable()]
+	[XmlRoot(ElementName = "catalog", Namespace = "http://library.by/catalog")]
+	public class Catalog : ISerializable
+	{
+		[XmlAttribute]
+		public string date { get; set; }
+		
+		//[XmlArrayItem("book")]
+		[XmlElement(ElementName ="book")]
+		public Books[] book { get; set; }
+
+
+		public Catalog()
+		{
+			
+		}
+		
+		public Catalog(string Date,Books[] Book)
+		{
+			date=Date;
+			book = Book;
+		}
+		public Catalog(SerializationInfo info, StreamingContext context)
+		{
+			
+			date = (string)info.GetValue("date", typeof(string));
+			book = (Books[])info.GetValue("book", typeof(Books[]));
+			
+		}
+
+		public void GetObjectData(SerializationInfo info, StreamingContext context)
+		{
+			if (info == null)
+				throw new System.ArgumentNullException("info");
+			info.AddValue("Date", date);
+			info.AddValue("Book",book);
+		}
+
+
+
+	}
+	[Serializable()]
+	public class Books : ISerializable
+	{
+		[XmlAttribute]
+		public string id { get; set; }
+		[XmlElement(ElementName = "isbn")]
+		public string Isbn { get; set; }
+		[XmlElement(ElementName = "author")]
+		public string Author { get; set; }
+		[XmlElement(ElementName = "title")]
+		public string Title { get; set; }
+		[XmlElement(ElementName = "genre")]
+		public GenreEnum Genre { get; set; }
+		[XmlElement(ElementName = "publisher")]
+		public string Publisher { get; set; }
+		[XmlElement(ElementName = "publish_date")]
+		public DateTime Publish_Date { get; set; }
+		[XmlElement(ElementName = "description")]
+		public string Description { get; set; }
+		[XmlElement(ElementName = "registration_date")]
+		public DateTime Registration_Date { get; set; }
+		public Books() { }
+		public Books(string id1, string isbn, string author, string title, GenreEnum genre, string publisher, DateTime publishDate, string description, DateTime registrationDate)
+		{
+			id = id1;
+			Isbn = isbn;
+			Author = author;
+			Title = title;
+			Genre = genre;
+			Publisher = publisher;
+			Publish_Date = publishDate;
+			Description = description;
+			Registration_Date = registrationDate;
+		}
+		public Books(SerializationInfo info, StreamingContext context)
+		{
+			id = (string)info.GetValue("id", typeof(string));
+			Isbn = (string)info.GetValue("Isbn", typeof(string));
+			Author = (string)info.GetValue("Author", typeof(string));
+			Title = (string)info.GetValue("Title", typeof(string));
+			Genre = (GenreEnum)info.GetValue("Genre", typeof(GenreEnum));
+			Publisher = (string)info.GetValue("Publisher", typeof(string));
+			Publish_Date = (DateTime)info.GetValue("Publish_Date", typeof(DateTime));
+			Description = (string)info.GetValue("Description", typeof(string));
+			Registration_Date = (DateTime)info.GetValue("Registration_Date", typeof(DateTime));
+		}
+		public void GetObjectData(SerializationInfo info, StreamingContext context)
+		{
+			info.AddValue("id", id);
+			info.AddValue("Isbn", Isbn);
+			info.AddValue("Author", Author);
+			info.AddValue("Title", Title);
+			info.AddValue("Genre", Genre);
+			info.AddValue("Publisher", Publisher);
+			info.AddValue("Description", Description);
+			info.AddValue("Registration_Date", Registration_Date);
+		}
+	}
+}
